3219bfa2f933cc712e2a804833ce2e1da82b3ce5
Alexey Vlasov <mail4turbo@gmail.com>
2025-01-15 11:21:18 +0700

    Fix SOURCE_TYPE_GPS constant which deprecated in Home Assistant 2025.1

--- custom_components/pandora_cas/device_tracker.py
+++ custom_components/pandora_cas/device_tracker.py
@@ -5,7 +5,7 @@ DETAILS
 """
 import logging
 
-from homeassistant.components.device_tracker import DOMAIN as PLATFORM_DOMAIN, SOURCE_TYPE_GPS
+from homeassistant.components.device_tracker import DOMAIN as PLATFORM_DOMAIN, SourceType
 from homeassistant.components.device_tracker.config_entry import TrackerEntity
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.helpers.typing import HomeAssistantType
@@ -73,7 +73,7 @@ class PandoraTrackerEntity(TrackerEntity):
     @property
     def source_type(self):
         """Return the source type, eg gps or router, of the device."""
-        return SOURCE_TYPE_GPS
+        return SourceType.GPS
 
     @property
     def icon(self):
